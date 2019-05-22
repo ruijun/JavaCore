829fca2d32f23e27db232254ac2d6a9fc91a3ca9
ruijun <liangruijun@huya.com>
2019-05-22 16:22:33 +0800

    update .gitignore

--- src/thread/join/JoinTest.java
+++ src/thread/join/JoinTest.java
@@ -14,8 +14,7 @@ public class JoinTest {
             if (i >= 2)
                 try {
                     // t1线程合并到主线程中，主线程停止执行过程，转而执行t1线程，直到t1执行完毕后继续。
-//                    thread.join();
-                    Thread.yield();
+                    thread.join();
                     System.out.println(Thread.currentThread().getName() + "==>" + thread.getState());
                 } catch (Exception e) {
                     e.printStackTrace();
